1d3dc854d07fe0dbf2cfa80aaa629b0ace1c6a0c
tuannt39 <tuannt39@gmail.com>
2019-02-22 13:47:42 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot Hello
+
+Spring Boot Hello
+
+- IDE: Intellij
+- https://stackjava.com/spring/code-vi-du-spring-boot-hello.html
---
+++ src/main/java/vn/teo/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package vn.teo.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "index";
+    }
+
+}
